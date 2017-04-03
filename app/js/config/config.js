b5b2ab0825eb83e2b68323f9ccf0bef1919b4024
boylittle <674728278@qq.com>
2017-04-03 14:04:38 +0800

    Update config.js

--- app/js/config/config.js
+++ app/js/config/config.js
@@ -1,5 +1,5 @@
-document.title ="http映射控制1";
-$("#head_msg").html("欢迎使用1");
+document.title ="http映射控制";
+$("#head_msg").html("欢迎使用");
 $("#foot_msg").html("made by weiyj, source  <a href='https://github.com/boylittle/app' target='_blank' >get source</a>");
 requiredApp("https://raw.githubusercontent.com/boylittle/app/master/app/js/config/js.js");
 requiredApp("https://raw.githubusercontent.com/boylittle/app/master/app/js/config/filter.js");
