04b51e4f39bf5793cdc5a3f1343308b9ffb0ed58
boylittle <674728278@qq.com>
2018-12-12 14:58:59 +0800

    Create cartPageRedesignForNewMobile.js

---
+++ b2c/cartPageRedesignForNewMobile.js
@@ -0,0 +1,6 @@
+{
+	".cartcheckout_summary_section":"summary main",
+	".cartcheckout_content":"summary",
+	"#checkoutForm-shippingContinueButton":"continue button",
+	"#firstName":"this is first name"
+}
