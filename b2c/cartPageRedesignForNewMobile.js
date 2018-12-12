4e345a84d0abfec5750b8697c5d185f465e02688
boylittle <674728278@qq.com>
2018-12-12 15:28:27 +0800

    Update cartPageRedesignForNewMobile.js

--- b2c/cartPageRedesignForNewMobile.js
+++ b2c/cartPageRedesignForNewMobile.js
@@ -2,5 +2,6 @@
 	".cartcheckout_summary_section":"summary main",
 	".cartcheckout_content":"summary",
 	"#checkoutForm-shippingContinueButton":"continue button",
-	"#firstName":"this is first name"
+	"#firstName":"this is first name",
+	"#orderSummarySection":"/nemostorefront/web/webroot/WEB-INF/views/desktop/pages/cart/cartPageRedesign.jsp"
 }
