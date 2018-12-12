f43f75794a55a3025c2ef8340590b4f0e04b072c
boylittle <674728278@qq.com>
2018-12-12 14:56:18 +0800

    Create learnConfig.js

---
+++ learnConfig.js
@@ -0,0 +1,9 @@
+{
+"b2c":{
+	"redesignCheckoutShippingBillingPage":"https://files.cnblogs.com/files/developer-ios/redesignCheckoutShippingBillingPage.js",
+	"cartPageRedesignForNewMobile":"https://files.cnblogs.com/files/developer-ios/cartPageRedesignForNewMobile.js"
+	}
+	
+,"b2b":{"redesignCheckoutShippingBillingPage":"https://files.cnblogs.com/files/developer-ios/redesignCheckoutShippingBillingPage.js"}
+		
+}
